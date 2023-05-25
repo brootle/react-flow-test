c78f9855b03d67129838c3ab023827b4decbb14f
Alexander Cherednichenko <luganskukraine@gmail.com>
2023-05-25 14:24:13 +0300

    tooltip as nodetoolbar added

--- 01_nodes_basics/src/DeafaultNode.js
+++ 01_nodes_basics/src/DeafaultNode.js
@@ -1,7 +1,9 @@
 import React, { memo, useState, useEffect, useRef } from 'react';
 import { useStoreApi, useReactFlow, Panel, MarkerType, getConnectedEdges, deleteElements } from 'reactflow';
 
-import { Handle } from 'reactflow';
+import { Handle, NodeToolbar, Position } from 'reactflow';
+
+// import { Tooltip } from 'react-tooltip' // see https://react-tooltip.com/docs/getting-started
 
 import Menu from './Menu';
 
@@ -11,6 +13,8 @@ export default memo(({id, data }) => {
 
   const [isEditing, setIsEditing] = useState(false);
   const [editedLabel, setEditedLabel] = useState(data.label);
+
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
 
   // Handle input change
   const handleInputChange = (e) => {
@@ -66,12 +70,52 @@ export default memo(({id, data }) => {
     setIsEditing(false);
   }
 
+
+  const selectNode = () => {
+
+    const { nodeInternals, edges } = store.getState();
+
+    const nodes = Array.from(nodeInternals).map(([, node]) => {
+        const normalizedNode = { ...node };
+        //delete normalizedNode.positionAbsolute;
+        return normalizedNode;    
+    });   
+    
+    // Check if the node with the specified ID already exists
+    const nodeExists = nodes.some((node) => node.id === id);
+
+    if (!nodeExists) {
+      console.log(`Node with ID ${id} is not found.`);
+      return;
+    }    
+
+    // Set the selected field of the specified node to true
+    const updatedNodes = nodes.map((node) => {
+      if (node.id === id) {
+        return {
+          ...node,
+          selected: true,
+          //selected: false,
+        };
+      }
+      //return node;
+      return {
+        ...node,
+        selected: false,
+      };
+    });       
+
+    setNodes([...updatedNodes]);
+
+  }
+
   return (
     <>
       <Handle 
         type="target"
       />
-      <div className='defaultNode'>
+      {/* <div className='defaultNode' data-tooltip-id={`node_tooltip_${id}`} onMouseEnter={selectNode}> */}
+      <div className='defaultNode' onMouseEnter={() => setIsTooltipVisible(true)} onMouseLeave={() => setIsTooltipVisible(false)}>
         { isEditing ?
           <div>
             <input className='nodrag nopan' type="text" value={editedLabel} onChange={handleInputChange} />
@@ -83,6 +127,30 @@ export default memo(({id, data }) => {
         }
         <Menu nodeId={id} handleEditClick={handleEditClick} />
       </div>
+
+      <NodeToolbar isVisible={isTooltipVisible} position={Position.Right}>
+        <div className='nodeTooltip'>
+          <h3>Tooltip for Node {id}</h3>
+          <p>Here's some interesting stuff:</p>
+          <ul>
+            <li>Some</li>
+            <li>Interesting</li>
+            <li>Stuff</li>
+          </ul>
+        </div>
+      </NodeToolbar>
+
+      {/* <Tooltip id={`node_tooltip_${id}`}>
+          <div>
+            <h3>Tooltip for Node {id}</h3>
+            <p>Here's some interesting stuff:</p>
+            <ul>
+              <li>Some</li>
+              <li>Interesting</li>
+              <li>Stuff</li>
+            </ul>
+          </div>
+      </Tooltip> */}
       <Handle 
         type="source"
       />
